fb698a1e2390c2b34872e0db555af5972722e26b
Divsatwork <divyansh.chopra31@gmail.com>
2020-05-12 23:20:25 +0530

    Updated products catalogue

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET product */
 router.get('/', function(req, res, next) {
-    console.log('Request received to add a product');
-  res.render('product_add', { title: 'Add product' });
+  res.render('product_main', { title: 'Products' });
 });
+
+//Return the add product page
+router.get('/add', function(req, res, next) {
+    res.render('product_add', {title: 'Add Product'});
+})
+
+//API to add a product
+router.post('/', function(req, res, next) {
+    console.log('Received request');
+    //Make call to the service layer after verifying all the information.
+    //If product add is success use the next to redirect to view product else return to add product
+}) 
 
 /*Export router means to export the routes to this page*/
 module.exports = router;

8866aa0aa08546117b6253e5fc4ef0d5549b07e7
Divsatwork <divyansh.chopra31@gmail.com>
2020-05-17 17:02:53 +0530

    Added service templates

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('../services/utilityService');
 
 /* GET product */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,7 @@ router.get('/', function(req, res, next) {
 
 //Return the add product page
 router.get('/add', function(req, res, next) {
-    res.render('product_add', {title: 'Add Product'});
+    res.render('product_add', {title: 'Add Product', error: ''});
 })
 
 //API to add a product
@@ -16,7 +17,15 @@ router.post('/', function(req, res, next) {
     console.log('Received request');
     //Make call to the service layer after verifying all the information.
     //If product add is success use the next to redirect to view product else return to add product
-}) 
+    if(!utils.verifyProductDetails(req.body)) {
+      
+    }
+})
+
+router.put('/', function(req, res, next) {
+  console.log('Updating values for Product');
+  //Make call to the service layer after verifying the info
+})
 
 /*Export router means to export the routes to this page*/
 module.exports = router;
---
+++ services/utilityService.js
@@ -0,0 +1,4 @@
+function verifyProductDetails(product) {
+    //Verify all the field values
+    //Return true if all good else false
+}
